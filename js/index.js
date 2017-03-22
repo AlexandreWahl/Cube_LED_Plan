d1233b355dae9ef44330429a854b6c814af3c280
Alexandre Wahl <alexandre.wahl@gmail.com>
2017-03-22 11:30:42 +0100

    11h30 - 22.03.2017

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var number = 16;
+var number = 20;
 var statusRotation = false;
 var statusShowAxes = false;
 
@@ -84,27 +84,45 @@ function setLedColor(x, y, z, c) {
     }
 }
 
+// TODO
 function confirmPlan() {
     turnOffAllLeds();
 
     var A = {
-        x : $("#0-0").find(":selected").val(),
-        y : $("#0-1").find(":selected").val(),
-        z : $("#0-2").find(":selected").val()
+        x : Math.floor((Math.random() * number)),
+        y : Math.floor((Math.random() * number)),
+        z : Math.floor((Math.random() * number))
     };
 
     var B = {
-        x : $("#1-0").find(":selected").val(),
-        y : $("#1-1").find(":selected").val(),
-        z : $("#1-2").find(":selected").val()
+        x : Math.floor((Math.random() * number)),
+        y : Math.floor((Math.random() * number)),
+        z : Math.floor((Math.random() * number))
     };
 
     var C = {
-        x : $("#2-0").find(":selected").val(),
-        y : $("#2-1").find(":selected").val(),
-        z : $("#2-2").find(":selected").val()
+        x : Math.floor((Math.random() * number)),
+        y : Math.floor((Math.random() * number)),
+        z : Math.floor((Math.random() * number))
     };
 
+    // var A = {
+    //     x : $("#0-0").find(":selected").val(),
+    //     y : $("#1-0").find(":selected").val(),
+    //     z : $("#2-0").find(":selected").val()
+    // };
+    //
+    // var B = {
+    //     x : $("#0-1").find(":selected").val(),
+    //     y : $("#1-1").find(":selected").val(),
+    //     z : $("#2-1").find(":selected").val()
+    // };
+    //
+    // var C = {
+    //     x : $("#0-2").find(":selected").val(),
+    //     y : $("#1-2").find(":selected").val(),
+    //     z : $("#2-2").find(":selected").val()
+    // };
 
     var u = {
         x : B.x - A.x,
@@ -125,7 +143,14 @@ function confirmPlan() {
     };
 
     var d = -(P.a * A.x + P.b * A.y + P.c * A.z);
-    var y = 0;
+
+    for(var i = 0;i < number;i++) {
+        for(var j = 0;j < number;j++) {
+            x = (P.b * i + P.c * j + d) / -P.a;
+            if(x > -1 && x < number)
+                setLedColor(Math.round(x), i, j, "red");
+        }
+    }
 
     for(var i = 0;i < number;i++) {
         for(var j = 0;j < number;j++) {
@@ -134,5 +159,45 @@ function confirmPlan() {
                 setLedColor(i, Math.round(y), j, "red");
         }
     }
+
+    for(var i = 0;i < number;i++) {
+        for(var j = 0;j < number;j++) {
+            z = (P.a * i + P.b * j + d) / -P.c;
+            if(z > -1 && z < number)
+                setLedColor(i, j, Math.round(z), "red");
+        }
+    }
+
+    $("#equation").text(P.a + "x + " + P.b + "y + " + P.c + "z + " + d + " = 0");
+}
+
+function confirmSphere()  {
+    turnOffAllLeds();
+
+    var C = {
+        x : Math.floor((Math.random() * number)),
+        y : Math.floor((Math.random() * number)),
+        z : Math.floor((Math.random() * number))
+    };
+
+    var r = Math.floor((Math.random() * number));
+
+
+
+    for(var i = 0;i < number;i+= 0.1) {
+        for(var j = 0;j < number;j+= 0.1) {
+            for(var k = 0;k < number;k+= 0.1) {
+                var nb = r * r + C.x * C.x + C.y * C.y + C.z * C.z;
+                var eq = i*i + 2 * (i* -C.x) + C.x * C.x + j*j + 2 * (j* -C.y) + C.y * C.y + k*k + 2 * (k* -C.z) + C.z * C.z - nb;
+
+                if(eq == 0) {
+                    setLedColor(Math.round(i), Math.round(j), Math.round(k), "red");
+                }
+            }
+        }
+    }
+
+
+
 
 }
