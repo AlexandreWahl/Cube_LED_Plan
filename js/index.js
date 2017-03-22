b61a2fbfde389a64bbcc5c7ed12386d27bb923b9
Alexandre Wahl <alexandre.wahl@gmail.com>
2017-03-22 08:57:51 +0100

    Finished

--- js/index.js
+++ js/index.js
@@ -1,62 +1,24 @@
-var number = 12;
+var number = 16;
 var statusRotation = false;
 var statusShowAxes = false;
 
 init(number);
 animate();
 
-var A = {
-    x : 1,
-    y : 4,
-    z : 8
-};
+for(var i = 0;i < 3;i++) {
+    for(var j = 0;j < 3;j++) {
+        var select = "<select id=" + i + "-" + j + ">";
+        select += "<option disabled selected></option>";
 
-var B = {
-    x : 1,
-    y : 2,
-    z : 4
-};
+        for(var k = 0; k<number;k++) {
+            select += "<option value=" + k + ">" + k + "</option>";
+        }
 
-var C = {
-    x : 5,
-    y : 2,
-    z : 4
-};
+        select += "</select>";
 
-
-var u = {
-    x : B.x - A.x,
-    y : B.y - A.y,
-    z : B.z - A.z
-};
-
-var v = {
-    x : C.x - A.x,
-    y : C.y - A.y,
-    z : C.z - A.z
-};
-
-var afterCross = {
-    a : u.y * v.z - u.z * v.y,
-    b : u.z * v.x - u.x * v.z,
-    c : u.x * v.y - u.y * v.x
-};
-
-var d = -(afterCross.a * A.x + afterCross.x * A.y + afterCross.c * A.z);
-
-console.log(d);
-
-var equation = afterCross.a
-
-for(var i = 0;i < number;i++) {
-    for(var j = 0;j < number;j++) {
-
+        $("#" + i + "-" + j).append(select);
     }
 }
-
-
-
-
 
 // Fonction permettant d'obtenir le statut de l'affichage des axes
 function getRotationStatus() {
@@ -113,4 +75,64 @@ function turnOffAllLeds() {
             }
         }
     }
+}
+
+// TODO
+function setLedColor(x, y, z, c) {
+    if(x < number && x >= 0 && y < number && y >= 0 && z < number && z >= 0) {
+        leds[x][y][z].setColor(c);
+    }
+}
+
+function confirmPlan() {
+    turnOffAllLeds();
+
+    var A = {
+        x : $("#0-0").find(":selected").val(),
+        y : $("#0-1").find(":selected").val(),
+        z : $("#0-2").find(":selected").val()
+    };
+
+    var B = {
+        x : $("#1-0").find(":selected").val(),
+        y : $("#1-1").find(":selected").val(),
+        z : $("#1-2").find(":selected").val()
+    };
+
+    var C = {
+        x : $("#2-0").find(":selected").val(),
+        y : $("#2-1").find(":selected").val(),
+        z : $("#2-2").find(":selected").val()
+    };
+
+
+    var u = {
+        x : B.x - A.x,
+        y : B.y - A.y,
+        z : B.z - A.z
+    };
+
+    var v = {
+        x : C.x - A.x,
+        y : C.y - A.y,
+        z : C.z - A.z
+    };
+
+    var P = {
+        a : u.y * v.z - u.z * v.y,
+        b : u.z * v.x - u.x * v.z,
+        c : u.x * v.y - u.y * v.x
+    };
+
+    var d = -(P.a * A.x + P.b * A.y + P.c * A.z);
+    var y = 0;
+
+    for(var i = 0;i < number;i++) {
+        for(var j = 0;j < number;j++) {
+            y = (P.a * i + P.c * j + d) / -P.b;
+            if(y > -1 && y < number)
+                setLedColor(i, Math.round(y), j, "red");
+        }
+    }
+
 }
